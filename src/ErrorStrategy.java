a95148c67fb7ea6e2cc67751ef278fd4637e958b
strawberryfg <wanqingfu@sina.com>
2016-11-24 19:51:51 +0800

    beginning of error strategy

---
+++ src/ErrorStrategy.java
@@ -0,0 +1,19 @@
+import org.antlr.v4.runtime.*;
+public class ErrorStrategy extends DefaultErrorStrategy
+{
+    @Override
+    public void recover(Parser recognizer, RecognitionException e)
+    {
+        throw new RuntimeException(e);
+    }
+
+    @Override
+    public Token recoverInline(Parser recognizer)
+            throws RecognitionException
+    {
+        throw new RuntimeException(new InputMismatchException(recognizer));
+    }
+
+    @Override
+    public void sync(Parser recognizer) { }
+}
